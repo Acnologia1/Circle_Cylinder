75df2e4592fc7cfefe9621e6a87e88e48e12e9f3
vancong <tavancong1994@gmail.com>
2019-05-21 05:45:09 +0700

    Create Circle01Test.java
    test

---
+++ Circle01Test.java
@@ -0,0 +1,11 @@
+package Lession4;
+
+public class Circle01Test {
+    public static void main(String[] args){
+        Circle01 circle = new Circle01();
+        System.out.println(circle);
+
+        circle = new Circle01(3.5," green1 ");
+        System.out.println(circle);
+    }
+}
